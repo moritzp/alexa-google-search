10d283974c48da225db0a98f95fd4358889dddb4
Moritz Prinz <moritz.prinz@tngtech.com>
2017-04-29 13:38:41 +0200

    missing ;

--- src/AlexaSkill.js
+++ src/AlexaSkill.js
@@ -19,7 +19,7 @@ function AlexaSkill(appId) {
 AlexaSkill.speechOutputType = {
     PLAIN_TEXT: 'PlainText',
     SSML: 'SSML'
-}
+};
 
 AlexaSkill.prototype.requestHandlers = {
     LaunchRequest: function (event, context, response) {
